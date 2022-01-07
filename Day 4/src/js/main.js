9a2003da3e6ecee26a5b2b98bb88327d976cdfa9
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-01-07 12:00:42 +0500

    filter function added

--- Day 4/src/js/main.js
+++ Day 4/src/js/main.js
@@ -1,24 +1,69 @@
 const users = [
-  { name: "John", email: "joh@mail.com", imgUrl: "./assets/img/john.png" }, //0
-  { name: "Smith", email: "smith@mail.com" }, //1
-  { name: "Doe", email: "doe@mail.com", imgUrl: "./assets/img/john.png" }, //2
+  {
+    name: "Diyora",
+    age: "18",
+    email: "joh@mail.com",
+    imgUrl: "./assets/img/john.png",
+  }, //0
+  {
+    name: "Salima",
+    age: "27",
+    email: "joh@mail.com",
+    imgUrl: "./assets/img/john.png",
+  }, //0
+  {
+    name: "Nodira",
+    age: "19",
+    email: "joh@mail.com",
+    imgUrl: "./assets/img/john.png",
+  }, //0
+  {
+    name: "Dono",
+    age: "35",
+    email: "joh@mail.com",
+    imgUrl: "./assets/img/john.png",
+  }, //0
+  {
+    name: "Madina",
+    age: "18",
+    email: "smith@mail.com",
+  }, //1
 ];
 
 const app = document.getElementById("app");
 
 for (let i = users.length - 1; i >= 0; i--) {
   const element = document.createElement("div");
-  element.classList.add("user");
-  element.insertAdjacentHTML(
-    "afterbegin",
-    `Name: ${users[i].name}<br/> Email: ${users[i].email}<br/>`
-  );
+  if (checkUser(users[i])) {
+    element.classList.add("user");
+    element.insertAdjacentHTML(
+      "afterbegin",
+      `Name: ${users[i].name}<br/> Email: ${users[i].email}<br/>`
+    );
 
-  if (users[i].imgUrl !== undefined) {
-    const image = document.createElement("img");
-    image.src = users[i].imgUrl;
-    image.classList.add("user-image");
-    element.insertAdjacentElement("beforeend", image);
+    if (users[i].imgUrl !== undefined) {
+      const image = document.createElement("img");
+      image.src = users[i].imgUrl;
+      image.classList.add("user-image");
+      element.insertAdjacentElement("beforeend", image);
+    }
+    const check = checkUser(users[i]);
+    console.log(check);
+    app.insertAdjacentElement("afterbegin", element);
   }
-  app.insertAdjacentElement("afterbegin", element);
+}
+
+function checkUser(user) {
+  let count = 0;
+  if (user.age >= 18 && user.age <= 35) {
+    count++;
+  }
+  if (user.imgUrl !== undefined) {
+    count++;
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
